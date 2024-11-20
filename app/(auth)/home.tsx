e829d6544b2eca6ebc6345b145f05e8fc34cda2b
Jeremy Odor <jeremyo5726@gmail.com>
2024-11-20 04:06:51 +0000

    Added Mood Logging

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -1,12 +1,21 @@
 import { View,Text, Button } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 import { auth } from '@/FirebaseConfig';
+import { useRouter } from "expo-router";
+
+
 
 const Page = () => {
     const user = auth.currentUser;
+    const router = useRouter();
+
     return (
         <View>
             <Text>Welcome {user?.email}</Text>
             <Button title="Sign Out" onPress={() => auth.signOut()} />
+            <Button title="Go to Mood Log" onPress={() => router.push('moodLog')} />
+    
+    
         </View>
     );
     };
