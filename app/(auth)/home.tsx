09ff2742a9e3c26fa6ff53376d0d6de48ad0eac0
Jeremy Odor <jeremyo5726@gmail.com>
2025-01-07 04:10:51 +0000

    Added Chatbot feature with message handling and navigation

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -15,6 +15,9 @@ const Page = () => {
             <Button title="Sign Out" onPress={() => auth.signOut()} />
             <Button title="Go to Mood Log" onPress={() => router.push('/(log)/moodLog')} />
             <Button title="Timetable" onPress={() => router.push('/(timetable)/table')} />
+            <Button title="Chatbot" onPress={() => router.push('/(chatbot)/chat')} />  
+                
+
     
     
     
