34e681d330cd5f8bbe193b72a8b1d18a189564f2
Jeremy2408 <jeremyo5726@gmail.com>
2025-04-05 12:15:23 +0100

    Add group invitation feature and improve group chat navigation

--- app/(chatroom)/groupChatRoom.tsx
+++ app/(chatroom)/groupChatRoom.tsx
@@ -1,9 +1,12 @@
+import { router } from 'expo-router';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
 const GroupChatRoom = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Pressable onPress={()=> router.back()}><Text>Go Back</Text></Pressable>
+        
       <Text> Group Chatroom Placeholder</Text>
     </View>
   );
